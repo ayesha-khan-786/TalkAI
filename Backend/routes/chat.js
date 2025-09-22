872f790cadee64ff36c6dd0e18a3d95825ce986f
Ayesha Khan <ayeshakhan61996@gmail.com>
2025-09-22 13:16:57 +0530

    Connected to DB

---
+++ Backend/routes/chat.js
@@ -0,0 +1,23 @@
+import express from "express";
+import Thread from "../models/Thread.js";
+
+const router = express.Router();
+
+// test
+router.post("/test", async(req, res) => {
+    try {
+        const thread = new Thread({
+            threadId: "abc",
+            title: "Testing new thread2"
+        });
+    
+        const response = await thread.save();
+        res.send(response);
+    
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({error: "Failed to save in DB"});
+    }
+});
+
+export default router;
