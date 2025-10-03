61ed5142b54f46746ba1ba370ac07e05de67a92d
Ayesha Khan <ayeshakhan61996@gmail.com>
2025-10-03 16:41:36 +0530

    Frontend setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css';
 import Sidebar from "./Sidebar.jsx";
 import ChatWindow from "./ChatWindow.jsx";
+import { MyContext } from './MyContext.jsx';
 
 function App() {
 
+  const providerValues = {};    // Passing values
+
   return (
-    <div>
+    <div className='main'>
+      <MyContext.Provider values = {providerValues}>
+        <Sidebar></Sidebar>
+        <ChatWindow></ChatWindow>
+      </MyContext.Provider>
     </div>
   )
 }
