ce55621da6b459c0b8150190bdd0be047ec6dcd5
Ayesha Khan <ayeshakhan61996@gmail.com>
2025-10-04 19:55:51 +0530

    Building Sidebar

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -2,9 +2,26 @@ import "./Sidebar.css";
 
 function Sidebar() {
     return ( 
-        <div>
-            Sidebar
-        </div>
+        <section className="sidebar">
+           {/* new chat button */}
+           <button>
+                <img src="src/assets/abstract.png" alt="talkAi logo"></img>
+                <i className="fa-solid fa-pen-to-square"></i>
+           </button>
+          
+           {/* History */}
+           <ul className="history">
+                <li>History1</li>
+                <li>History2</li>
+                <li>History3</li>
+                <li>History4</li>
+           </ul>
+           
+           {/* Sign */}
+           <div className="sign">
+                <p>By Aisha Khan &hearts;</p>
+           </div>
+        </section>
      );
 }
 
