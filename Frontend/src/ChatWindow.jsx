d8570529f1f3a5761d14626da3cbebd796a0c665
Ayesha Khan <ayeshakhan61996@gmail.com>
2025-10-05 12:13:00 +0530

    Styled ChatWindow

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,9 +1,30 @@
 import "./ChatWindow.css";
+import Chat from "./Chat.jsx";
 
 function ChatWindow() {
     return ( 
-        <div>
-            ChatWindow
+        <div className="chatWindow">
+            <div className="navbar">
+                    <span>TalkAi <i class="fa-solid fa-chevron-down"></i></span>
+                <div className="userIconDiv">
+                    <span className="userIcon"><i class="fa-solid fa-user"></i></span>
+                </div>
+            </div>
+            
+            <Chat></Chat>
+           
+            <div className="chatInput">
+                <div className="inputBox">
+                    <input placeholder="Ask anything" className="input">
+
+                    </input>
+                    <div id="submit"><i class="fa-solid fa-paper-plane"></i></div>
+                </div>
+
+                <p className="info">
+                    TalkAi can make mistakes. Check important info. See Cookie Preferences.
+                </p>
+            </div>
         </div>
      );
 }
