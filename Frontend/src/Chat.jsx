12864fcb7881227e3f9e4f801b25a9332fbf4445
Ayesha Khan <ayeshakhan61996@gmail.com>
2026-02-08 14:13:23 +0530

    Added Typing Effect

--- Frontend/src/Chat.jsx
+++ Frontend/src/Chat.jsx
@@ -1,21 +1,42 @@
 import "./Chat.css";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { MyContext } from "./MyContext";
 import ReactMarkdown from "react-markdown";
 import rehypeHightlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
 
 // react-markdown => display info in proper format
 // rehype-highlight => used for code syntax hightlight
 
 function Chat() {
 
-    const {newChat, prevChats} = useContext(MyContext); 
+    const {newChat, prevChats, reply} = useContext(MyContext);
+    const [latestReply, setLatestReply] = useState(null); 
+    
+    useEffect(() => {
+        // latestReply separate => create typing effect
+        if(!prevChats?.length) return;
+
+        const content = reply.split(" ");       // store individual words
+
+        let idx = 0;
+        const interval = setInterval(() => {
+            setLatestReply(content.slice(0, idx+1).join(" "));      // print separate separate word
+
+            idx++;
+            if(idx >= content.length) clearInterval(interval);
+        }, 40);
+
+        return () => clearInterval(interval);
+
+    }, [prevChats, reply]);
+
     return ( 
         <>
             {newChat && <h1>Ask Anything, Learn Everything!</h1>}
             <div className="chats">
                 {
-                    prevChats?.map((chat, idx) => 
+                    prevChats?.slice(0, -1).map((chat, idx) => 
                         <div className={chat.role === "user" ? "userDiv" : "aiDiv"} key={idx}>
                             {
                                 chat.role === "user"? 
@@ -28,19 +49,15 @@ function Chat() {
                         </div>
                     )
                 }
+    {/* To Print latest Reply */}
+                {
+                    prevChats.length > 0 && latestReply !== null &&
+                    <div className="aiMessage" key={"typing"}>
+                        <ReactMarkdown rehypePlugins={[rehypeHightlight]}>{latestReply}</ReactMarkdown>
+                    </div>
+                    
+                }
 
-{/* 
-                <div className="userDiv">
-                    <p className="userMessage">
-                        User Message
-                    </p>
-                </div>
-                
-                <div className="aiDiv">
-                    <p className="aiMessage">
-                        Ai generated Message
-                    </p>
-                </div>              */}
             </div>
         </>
      )
